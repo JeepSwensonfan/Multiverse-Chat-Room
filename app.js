04148b00a37f6f1c15cfef9af686518c60be3594
JeepSwensonfan <tommymrobinson1980@gmail.com>
2025-03-09 17:18:27 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://multiverse-chat-room.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
